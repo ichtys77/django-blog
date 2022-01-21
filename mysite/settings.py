6deae969b15c2c5d920da7b9e247794ee114d022
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2022-01-21 15:06:07 +0100

    CHANGE allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tgjs#!)bf@qdexypu85!bl^(o7+%#3zi&osn6li(!ee1wru*^g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ichtys77.pythonanywhere.com']
 
 
 # Application definition
